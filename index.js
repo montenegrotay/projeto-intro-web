c84477cd8d132951dfe5410ad1dd3a5397851a62
Taynara <montenegro.taynara@gmail.com>
2022-09-22 17:33:57 -0300

    Projeto Semana 3

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-//SEMANA 03
-//1. Reescrevendo o relatório criado utilizando console.log() que começamos na semana 2, altere a forma que a característica de array dos itens seja escrita como um laço que guarde todos os valores da propriedade array do objeto em uma mesma string. Utilize esta string no relatório.
+// SEMANA 03
+//1. Reescrevendo o relatório criado utilizando console.log() que começamos na semana 2,
+//altere a forma que a característica de array dos itens
+//seja escrita como um laço
+//que guarde todos os valores da propriedade array do objeto
+//em uma mesma string. 
+//Utilize esta string no relatório.
 //💡 Chamamos este processo de reescrever um código já escrito, de refatoração.
-
 //Exemplo:
 
 //ANTES 
@@ -11,61 +15,87 @@
 //projetos: ["Projeto de HTML", "Projeto de CSS", "Projeto React"] 
 
 //DEPOIS
-//FULANO
+//FULANO 
 //idade: 33
 //verificou email?: false
 //projetos: "Projeto de HTML,Projeto de CSS,Projeto React" 
 
-const objeto1 = {
-    módulo1: "Legislação de Trânsito",
-    cargaHorária1: 12,
-    avaliação1: true 
- }
+console.log("============== EX01 ============")
+
+for (i = 0; i <todosOsObjetos.length; i++) {
+   console.log("Módulo: "+ todosOsObjetos[i].módulo.toUpperCase())
+   console.log("Carga Horária: "+todosOsObjetos[i].cargaHorária)
+   console.log("Avaliação: "+todosOsObjetos[i].avaliação)
+   console.log("Dados necessários: "+todosOsObjetos[i].dadosDoUsuário.join(", "))
+}
+
+//2. Ainda no relatório, altere-o para que ele seja criado utilizando laços.
+//Ou seja, você não deve mais imprimir individualmente cada item do relatório.
+//Cada item deve ser exibido a partir de uma iteração do laço.
+//Para testar, adicione mais um item ao array de objetos, e verifique se ele é exibido corretamente.
+
+//let cars = [
+//   {
+//     "color": "purple",
+//     "type": "minivan",
+//     "registration": new Date('2017-01-03'),
+//     "capacity": 7
+//   },
+//   {
+//     "color": "red",
+//     "type": "station wagon",
+//     "registration": new Date('2018-03-03'),
+//     "capacity": 5
+//   }
+// ];
  
- const objeto2 = {
-   módulo2: "Direção Defensiva",
-   cargaHorária2: 8,
-   avaliação2: true
- }
+ //ANTES 
+// console.log("color: "+cars[0].color);
+// console.log("type: "+cars[0].type);
+// console.log("registration: "+cars[0].registration);
+// console.log("capacity: "+cars[0].capacity);
  
- const objeto3 = {
-  módulo3: "Noções de Primeiros Socorros",
-  cargaHorária3: 4,
-  avaliação3: true,
- }
+// console.log("----------------------");
  
- const objeto4 = {
-  módulo4: "Relacionamento Interpessoal",
-  cargaHorária4: 6,
-  avaliação4: true
+// console.log("color: "+cars[1].color);
+// console.log("type: "+cars[1].type);
+// console.log("registration: "+cars[1].registration);
+// console.log("capacity: "+cars[1].capacity);
+ 
+// //DEPOIS
+// for(car of cars){
+//     for (propriedade in car){
+//         console.log(`${propriedade}: ${car[propriedade]}`)
+//     }
+// console.log("----------------------");
+// }
 
 
-  const todosOsObjetos = []
-  console.log(todosOsObjetos)
-
-  todosOsObjetos.push(objeto1, objeto2, objeto3, objeto4)
+//3. Crie uma função que receba como parâmetro um objeto,
+//e devolva a string do relatório com os dados do objeto.
 
 
-  if(checarAvaliação == true) {
-    console.log("Você foi Aprovado!", todosOsObjetos.push())
-    /*Se todas as avaliações forem true, o usuário foi aprovado*/
- } else if (checarAvaliação == false) {
-    console.log(alert("Você foi reprovado, refaça suas avaliações!"))
-    /*Se alguma avaliação for false, o usuário está reprovado*/
- }
-
-//2. Ainda no relatório, altere-o para que ele seja criado utilizando um laço. Ou seja, você não deve mais imprimir individualmente cada item do relatório. Cada item deve ser exibido a partir de uma iteração do laço. Para testar, adicione mais um item ao array de objetos, e verifique se ele é exibido corretamente.
-//ANTES 
-//console.log(elementos[0].item);
-
-//DEPOIS
-//FULANO
-//for(elemento in elementos){
-//  console.log(elemento.item);
-//}
+for(cronograma of todosOsObjetos) {
+   for (propriedade in cronograma) {
+      console.log(`${propriedade}: ${cronograma[propriedade]}`)
+   }
+   console.log("----------------------------------------------")
+}
 
 
-//3. Crie uma função que receba como parâmetro um objeto, e devolva a string do relatório com os dados do objeto.
+function todosOsCursosDeReciclagem (adicionandoDados) {
+   dadosDoUsuário.push('CPF') //Adicionando mais uma informação a dadosDoUsuário: CPF.
+   console.log(dadosDoUsuário)
+}
+todosOsCursosDeReciclagem (dadosDoUsuário[i])
 
 
-//4. Crie uma função que recebe um array de objetos e uma string. Esta função deve retornar um objeto, e o objeto retornado deve possuir apenas os itens que tenham o nome/título igual à string passada como parâmetro. Caso não exista um item, exiba um ALERT indicando que nenhum item foi encontrado.
+
+//4. Crie uma função que recebe um array de objetos e uma string.
+//Esta função deve retornar um objeto, e o objeto retornado deve possuir apenas os itens que tenham o nome/título igual à string passada como parâmetro.
+//Caso não exista um item,
+//exiba um ALERT indicando que nenhum item foi encontrado
+
+const filtro = todosOsObjetos.filter(function(dados) {
+   return dadosDoUsuário[i] === dadosDoUsuário[CPF] ? console.log("Documento encontrado.") : alert("Item não encontrado.")
+})
